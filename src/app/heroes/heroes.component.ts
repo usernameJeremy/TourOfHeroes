dbf44efc8a62736bb6d6dfc01d880a3e8a36b9d3
usernameJeremy <Jeremy_koster@outlook.com>
2023-03-14 16:50:15 +0100

    Mock data en weergave ljist styling v/d button

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,14 @@ export class HeroesComponent {
   
   hero: Hero ={
     id: 1,
-    name: 'Windstorm,'
+    name: 'Windstorm'
+  }
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero:Hero): void {
+    this.selectedHero = hero;
   }
 
 }
