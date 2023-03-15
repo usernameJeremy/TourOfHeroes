cc03d375057680cba55738d0927cc1bbfe9f1735
usernameJeremy <Jeremy_koster@outlook.com>
2023-03-15 10:51:25 +0100

    hfdstuk add services klaar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailsComponent
+    HeroDetailsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -17,6 +18,7 @@ export class HeroesComponent implements OnInit{
 //constructor injection mag enkel injectie bevatten
 constructor(
   private heroService: HeroService,
+  private messageService: MessageService,
 ){}
 
 
@@ -26,6 +28,8 @@ ngOnInit(): void {
 
   onSelect(hero:Hero): void {
     this.selectedHero = hero;
+    //backticks en  ${} zorgt er voor dat je dynamische info kan laten zien in een string/message 
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 //waits for the Observable to emit the array of heroes
 // subscribe() method passes the emitted array to the callback, sets component's heroes property.
