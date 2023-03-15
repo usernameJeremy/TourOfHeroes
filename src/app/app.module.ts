f25d564962d795378b86baae25e27820da8e3511
usernameJeremy <Jeremy_koster@outlook.com>
2023-03-15 08:42:26 +0100

    modified:   src/app/app.module.ts 	new file:   src/app/hero-details/hero-details.component.html 	new file:   src/app/hero-details/hero-details.component.scss 	new file:   src/app/hero-details/hero-details.component.spec.ts 	new file:   src/app/hero-details/hero-details.component.ts 	modified:   src/app/heroes/heroes.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailsComponent
   ],
   imports: [
     BrowserModule,
